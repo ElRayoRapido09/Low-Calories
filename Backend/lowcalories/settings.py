62ec7d2a225e128706d383ac62604fdcdba5dcf2
Bellion <itanxolalpa2@gmail.com~>
2025-10-16 19:55:00 -0600

    Use api FastSecret

--- Backend/lowcalories/settings.py
+++ Backend/lowcalories/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'accounts',
     'foods',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
+
